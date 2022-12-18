326a533ce6e5d3738828eb7e049483c9ee871ab4
Prasannapediredla <119720248+Prasannapediredla@users.noreply.github.com>
2022-12-18 23:01:24 +0530

    #include <stdio.h> #include <math.h> int main() {     int num,a;     float fVar;     scanf("%d",&num);   fVar=sqrt((double)num);     a=fVar;  if(a==fVar)         printf("True");     else         printf("False"); }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num,a;
+    float fVar;
+    scanf("%d",&num);
+  fVar=sqrt((double)num);
+    a=fVar;
+ if(a==fVar)
+        printf("True");
+    else
+        printf("False");
+}
